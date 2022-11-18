6aad2c961ac99ff749722bb687ce092f02d76a23
Praseem Singh Beniwal <p.beniwal@student.unsw.edu.au>
2022-11-18 18:09:36 +1100

    ml progress

--- machine_learning/training.py
+++ machine_learning/training.py
@@ -1,7 +1,20 @@
 import pywt
-from segmentation import segmented_heartbeats
-from feature_extraction import dwt_decompose
+# from segmentation import segmented_heartbeats
+# from feature_extraction import dwt_decompose
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.interpolate
+import pandas as pd
+from sklearn.svm import SVC
+
+clf = SVC(kernel='linear')
+
+x = pd.read_csv("temp2.csv")
+a = np.array(x)
+y  = np.array(x["Type"])
+x = np.array(x[1:57])
+
+clf.fit(x, y)
+
+print(x.shape)
 
